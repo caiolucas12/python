316fc599afffaaab9c505c4b55010c46f39f0eb8
Caio <caiolucas2006@gmail.com>
2025-09-30 12:04:36 -0300

    final com def

--- classe_Conta.py
+++ classe_Conta.py
@@ -2,10 +2,20 @@ class Conta:
     numero: int = 35017
     saldo: float = 15.000
     tipo: str = 'Salário'
-banco = Conta()   #ou banco = Conta
 
-banco.numero
-banco.saldo
-print(f'Número: {banco.numero}')
-print(f'Saldo: {banco.saldo}')
-print(f'Tipo: {banco.tipo}')
+    def __init__(self, numero, saldoInicial):
+        self.numero = numero
+        self.saldo = saldoInicial
+
+    def deposito(self, valor):
+        self.saldo += valor
+
+    def saque(self, valor):
+        if (self.saldo > 0):
+            self.saldo -= valor
+        else:
+            print('ta pobre')
+#main
+conta = Conta('3501-7', 10000)
+print(conta.numero)
+print(conta.saldo)
